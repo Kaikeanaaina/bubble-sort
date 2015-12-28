a67d6ee5dd7ab8d71b5bded8039338992acd482d
KaiKeanaaina <kaikeanaaina@gmail.com>
2015-12-28 11:34:43 -1000

    set up test

--- test/bubble-sort.spec.js
+++ test/bubble-sort.spec.js
@@ -2,3 +2,10 @@ var chai = require('chai');
 var expect = chai.expect;
 chai.should();
 var bubbleSort = require('./../index.js');
+
+describe('bubblesort',function(){
+  it('should be function', function(){
+    expect(bubbleSort).to.be.a('function');
+  });
+
+});
